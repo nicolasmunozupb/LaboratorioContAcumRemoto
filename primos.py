7938ce5704832044e8c46bbdc343f02d52c1dcfa
Munoz Vega <000400505@bga.upb>
2019-03-08 13:45:49 -0500

    ejercicio resuelto

--- primos.py
+++ primos.py
@@ -1,6 +1,9 @@
 a=int(input("digite un numero"))
-
-for i in range(1,a):
-
-    i=a%i
-    print(i)
+div=0
+for i in range(1,a+1):
+    if a%i==0:
+        div=div+1
+if div>2:
+    print("el numero no es primo")
+else:
+    print("el numero es primo")
