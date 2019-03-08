16c53253adbc8700fa2a51287ef7b502ec5e07c0
Munoz Vega <000400505@bga.upb>
2019-03-08 12:42:29 -0500

    Mi primer commit

---
+++ primos.py
@@ -0,0 +1,6 @@
+a=int(input("ingrese un numero :a:"))
+b=int(input("ingrese otro numero :b:"))
+if a%b==0:
+    input("el numero es dibisible entre b")
+else:
+    input("a no es divisible entre b")
