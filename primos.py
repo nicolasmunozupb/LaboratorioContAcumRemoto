83555a8313caef84baddf666d8ed7aabc0d568fc
Munoz Vega <000400505@bga.upb>
2019-03-08 13:35:15 -0500

    ya casi calculo un primo

--- primos.py
+++ primos.py
@@ -1,6 +1,186 @@
-a=int(input("ingrese un numero :a:"))
-b=int(input("ingrese otro numero :b:"))
-if a%b==0:
-    input("el numero es dibisible entre b")
-else:
-    input("a no es divisible entre b")
+Python 3.4.0 (v3.4.0:04f714765c13, Mar 16 2014, 19:24:06) [MSC v.1600 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+ingrese un numero :a:10
+ingrese otro numero :b:2
+el numero es dibisible entre b
+>>> ================================ RESTART ================================
+>>> 
+ingrese un numero :a:11
+ingrese otro numero :b:2
+a no es divisible entre b
+>>> ================================ RESTART ================================
+>>> 
+1
+1
+2
+3
+4
+
+
+2
+1
+2
+3
+4
+
+
+3
+1
+2
+3
+4
+
+
+>>> ================================ RESTART ================================
+>>> 
+1
+1
+1
+1
+2
+1
+3
+1
+4
+
+
+2
+2
+1
+2
+2
+2
+3
+2
+4
+
+
+3
+3
+1
+3
+2
+3
+3
+3
+4
+
+
+>>> ================================ RESTART ================================
+>>> 
+ingrese un valor limite 5
+0
+>>> ================================ RESTART ================================
+>>> 
+ingrese un valor limite4
+5
+>>> ================================ RESTART ================================
+>>> 
+ingrese un valor limite4
+5
+5
+>>> ================================ RESTART ================================
+>>> 
+digite un numero10
+digite otro valor2
+1
+1
+1
+1
+1
+1
+1
+1
+1
+>>> ================================ RESTART ================================
+>>> 
+digite un numero010
+digite otro valor5
+1
+1
+1
+1
+1
+1
+1
+1
+1
+>>> ================================ RESTART ================================
+>>> 
+digite un numero10
+digite otro valor5
+2
+3
+4
+5
+6
+7
+8
+9
+0
+>>> ================================ RESTART ================================2
+>>> 
+digite un numero020
+digite otro valor10
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+0
+>>> ================================ RESTART ================================
+>>> 
+digite un numero5
+digite otro valor2
+2
+3
+4
+0
+>>> ================================ RESTART ================================
+>>> 
+digite un numero10
+digite otro valor10
+2
+3
+4
+5
+6
+7
+8
+9
+0
+>>> ================================ RESTART ================================
+>>> 
+digite un numero10
+Traceback (most recent call last):
+  File "C:/Users/000400505/Desktop/LaboratorioContAcum/prueba.py", line 3, in <module>
+    for i in range(i,a):
+NameError: name 'i' is not defined
+>>> ================================ RESTART ================================
+>>> 
+digite un numero10
+0
+0
+1
+2
+0
+4
+3
+2
+1
+>>> 
